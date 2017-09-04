1374e66e49b5e84d036f3bd517ad94813dad9468
David Smith <david.smith@gci.net>
2017-09-04 10:27:59 -0800

    Add files via upload

---
+++ src/models/users/decorators.py
@@ -0,0 +1,14 @@
+from functools import wraps
+
+from flask import session, url_for, request
+from werkzeug.utils import redirect
+
+
+def requires_login(func):
+    @wraps(func)
+    def decorated_func(*args, **kwargs):
+        if 'email' not in session.keys() or session['email'] is None:
+            return redirect(url_for('users.login_user', next=request.path))
+        return func(*args, **kwargs)
+    return decorated_func
+
